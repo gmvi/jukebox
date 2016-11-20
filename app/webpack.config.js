534e97c8a2cf3645526bb6d88a4778e152ea7246
George Matter <george.matter.vi@gmail.com>
2016-11-20 01:05:16 -0500

    mucking about with structure some more

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,21 +5,21 @@ var _       = require('lodash'),
 
 module.exports = {
   entry: [
-    path.join(__dirname, './app/web/entry.jsx') // your app's entry point
+    path.join(__dirname, './web/entry.jsx') // your app's entry point
   ],
   devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
   output: {
-    path: path.join(__dirname, 'app', 'assets'),
+    path: path.join(__dirname, 'assets'),
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
     alias: {
       react: 'react/addons',
-      shared: path.join(__dirname, 'app', 'shared'),
-      "static": path.join(__dirname, 'app', 'static'),
-      web: path.join(__dirname, 'app', 'web'),
-      components: path.join(__dirname, 'app', 'web/components')
+      shared: path.join(__dirname, 'shared'),
+      "static": path.join(__dirname, 'static'),
+      web: path.join(__dirname, 'web'),
+      components: path.join(__dirname, 'web/components')
     }
   },
   module: {
